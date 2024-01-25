c31084b4425bd82f80e86a2b7a049b97e67b482a
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2024-01-25 11:54:48 +0530

    import java.util.*; public class MyClass {     public static void main(String[] args)     {         Scanner scan = new Scanner(System.in);         int a = scan.nextInt();         if(a > 98)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Fever.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        if(a > 98)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
